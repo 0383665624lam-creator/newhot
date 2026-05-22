8089ce16f4cdca42c1813d9d72d5e3feffa76e08
Creating <0383665624Lam@gmail.com>
2026-05-22 19:01:34 +0700

    first commit

---
+++ Javascript.js
@@ -0,0 +1,7 @@
+function turnOn() {
+  document.getElementById("myImage").src = "img/pic_bulbon.gif";
+}
+
+function turnOff() {
+  document.getElementById("myImage").src = "img/pic_bulboff.gif";
+}
